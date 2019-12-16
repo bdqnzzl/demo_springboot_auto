8829c4a567cfcdaa16f3155fc500d98bedc93431
zzlling123 <bdqn_zzl@163.com>
2019-12-16 17:22:07 +0800

    first

---
+++ src/test/java/com/bdqn/demo_springboot_auto/DemoSpringbootAutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdqn.demo_springboot_auto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootAutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
